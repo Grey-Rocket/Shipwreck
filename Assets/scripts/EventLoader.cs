a756fb063a71975c94ce0fb44d761db124612705
whiterocket <aljaz.podgornik@t-2.net>
2019-02-23 00:25:52 +0100

    added a scene for events and started on event preset

---
+++ Assets/scripts/EventLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EventLoader : MonoBehaviour
+{
+
+    public Text eventText;
+    public Canvas eventCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        eventText.text = "In the eye.In the eye.In the eye.In the eye.In the eye.In the eye." +
+            "In the eye.In the eye.In the eye.In the eye.In the eye.In the eye.";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
