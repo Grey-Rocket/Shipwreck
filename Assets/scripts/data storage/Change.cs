bdab80b3aca554e156d357a20ff89a038269f4f4
whiterocket <aljaz.podgornik@t-2.net>
2019-02-25 00:52:14 +0100

    added hierarchy of files: event-> choice-> change

---
+++ Assets/scripts/data storage/Change.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Change : ScriptableObject
+{
+    public float percentage;
+    public string fieldOfChange;
+    public int ammount;
+}
