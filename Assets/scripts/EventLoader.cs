c2531a0b08840634b31f69e258bd9af6c5cbfe47
whiterocket <aljaz.podgornik@t-2.net>
2019-03-04 23:12:25 +0100

    trying some weird things

--- Assets/scripts/EventLoader.cs
+++ Assets/scripts/EventLoader.cs
@@ -10,6 +10,10 @@ public class EventLoader : MonoBehaviour
     public Canvas eventCanvas;
     public Event currentEvent;
 
+    public Button button1;
+    public Button button2;
+    public Button button3;
+
     // Start is called before the first frame update
     void Start()
     {
